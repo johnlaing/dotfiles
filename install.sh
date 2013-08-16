8be172b988b63e496eac7a85cee36499937ee5ae
John Laing <john.laing@gmail.com>
2013-08-16 14:40:26 -0400

    make install script work with directories

--- install.sh
+++ install.sh
@@ -7,18 +7,25 @@ if [ $0 != "./install.sh" ]; then
   exit 1
 fi
 
-## install script borrowed from thoughtbot
+## install script borrowed from thoughtbot (initially, anyway)
 ## https://github.com/thoughtbot/dotfiles/blob/master/install.sh
-for name in *; do
-  target="$HOME/.$name"
-  if [ -e "$target" ]; then
-    if [ ! -L "$target" ]; then
-      echo "WARNING: $target exists but is not a symlink."
-    fi
-  else
-    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
+install() {
+  BASE="$1"
+  for name in "$BASE"*; do
+    target="$HOME/.$name"
+    if [ "$name" = 'install.sh' ] || [ "$name" = 'README.md' ] || [ -L "$target" ]; then
+      ## nothing: ignore
+      echo -n ""
+    elif [ ! -e "$target" ]; then
       echo "Creating $target"
       ln -s "$PWD/$name" "$target"
+    elif [ -d "$name" ]; then
+      echo "Recursing into $name/"
+      install "$name/"
+    else
+      echo "WARNING: $target exists but is not a symlink or a directory."
     fi
-  fi
-done
+  done
+}
+
+install
